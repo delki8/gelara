0f5ce95082617f1dcf77795b6507e53603ee5726
ruither.borba <ruither.borba@tecsinapse.com.br>
2017-09-01 20:14:38 -0300

    adding icons and refactoring the music card

--- imports/ui/Music.jsx
+++ imports/ui/Music.jsx
@@ -14,39 +14,36 @@ export default class Music extends Component {
   }
 
   render() {
-    const cardStyle = {
-      marginTop: '0.5girem',
-      backgroundColor: '#FFFFFF',
-      // borderSize: '3px',
-      borderStyle: 'solid',
-      borderColor: '#2F4F4F',
-      borderRadius: '5px',
-    };
     // const times = this.props.msc.timesPlayed > 1 ? ' times' : ' time';
     // const timesPlayedMsg = this.props.msc.timesPlayed ?
     // 'This song has been played ' + this.props.msc.timesPlayed + times :
     // 'This song have never been played';
     return (
-      <div className="row" style={cardStyle}>
-        <div className="col-2">
-
-          {this.props.msc.timesPlayed}
-        </div>
-        <div className="col-6">
-          {this.props.msc.name}
-        </div>
-        <div className="col-2">
+      <div className="row musicCardStyle">
+        <div className="col-1 removeStyle">
           <a href="#"
-            className="btn btn-primary btn-sm"
-            onClick={this.addMusicToSunday.bind(this)}>
-            add
+            className="btn rounded-circle removeButton"
+            onClick={this.deleteMusic.bind(this)}>
+            <span className="oi oi-trash"
+                    title="add"
+                    aria-hidden="true"></span>
           </a>
         </div>
-        <div className="col-2">
+        <div className="col-2 counterColumnStyle">
+          <span className="counterStyle">
+            {this.props.msc.timesPlayed ? this.props.msc.timesPlayed : 0}
+          </span>
+        </div>
+        <div className="col-6 nameStyle">
+          {this.props.msc.name}
+        </div>
+        <div className="col-2 addStyle">
           <a href="#"
-            className="btn btn-danger btn-sm float-right"
-            onClick={this.deleteMusic.bind(this)}>
-            X
+            className="rounded-circle addButton"
+            onClick={this.addMusicToSunday.bind(this)}>
+            <span className="oi oi-plus"
+                    title="add"
+                    aria-hidden="true"></span>
           </a>
         </div>
       </div>
